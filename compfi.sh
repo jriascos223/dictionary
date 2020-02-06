7e271db87996c1db859c57c09e1403fb479b5e96
Joseph Riascos <josephriascos@gmail.com>
2020-02-06 16:05:32 -0500

    renamed compliation script, new test window

--- compfi.sh
+++ compfi.sh
@@ -7,13 +7,14 @@ then
     unzip -nq openjfx-11.0.2_windows-x64_bin-sdk
 else
     unzip -nq javafx-sdk-11.0.2.zip
+    rm -rf __MACOSX
 fi
 
-echo "Unzipped JavaFX"
+echo "Unzipped JavaFX."
 jfxpath='util/javafx-sdk-11.0.2/lib'
 cd ..
 javac -d bin --module-path $jfxpath --add-modules javafx.controls src/tech/jriascos/*/*.java
 echo "Compiled project."
-echo "Running main found in tech.jriascos.application.Window"
-java -classpath $dir/bin --module-path $jfxpath --add-modules javafx.controls tech.jriascos.application.Window
+echo "Running main function found in tech.jriascos.application.Window"
+java -classpath $dir/bin:$dir/util/gson-2.8.2.jar --module-path $jfxpath --add-modules javafx.controls tech.jriascos.application.Window
 rm -rf util/javafx-sdk-11.0.2
--- src/tech/jriascos/application/Window.java
+++ src/tech/jriascos/application/Window.java
@@ -1,37 +1,57 @@
 package tech.jriascos.application;
  
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.animation.FillTransition; 
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Rectangle; 
+import javafx.stage.Stage; 
+import javafx.animation.Timeline; 
+import javafx.animation.ParallelTransition; 
+import javafx.animation.RotateTransition; 
+import javafx.animation.ScaleTransition; 
+import javafx.animation.TranslateTransition; 
+import javafx.util.Duration;
  
 public class Window extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+   public void start(Stage stage) {
+        //Scene and root node setup
+        Group root = new Group(); 
+        Scene scene = new Scene(root, 500, 500, Color.BLACK); 
+        Rectangle r = new Rectangle(0, 0, 250, 250); 
+        r.setFill(Color.BLUE); 
+        root.getChildren().add(r); 
  
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
- public static void main(String[] args) {
-        launch(args);
-    }
+        TranslateTransition translate = 
+        new TranslateTransition(Duration.millis(750)); 
+        translate.setToX(390); 
+        translate.setToY(390); 
+ 
+        FillTransition fill = new FillTransition(Duration.millis(750)); 
+        fill.setToValue(Color.RED); 
+ 
+        RotateTransition rotate = new RotateTransition(Duration.millis(750)); 
+        rotate.setToAngle(360); 
+ 
+        ScaleTransition scale = new ScaleTransition(Duration.millis(750)); 
+        scale.setToX(0.1); 
+        scale.setToY(0.1); 
+ 
+        ParallelTransition transition = new ParallelTransition(r, 
+        translate, fill, rotate, scale); 
+        transition.setCycleCount(Timeline.INDEFINITE);
+        transition.setAutoReverse(true); 
+        transition.play(); 
+ 
+        stage.setTitle("JavaFX Scene Graph Demo"); 
+        stage.setScene(scene); 
+        stage.show(); 
+   }
+ 
+   public static void main(String[] args) {
+       launch(args);
+   }
 }
