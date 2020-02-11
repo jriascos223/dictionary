c35734132249cbc648f0254c6bab65f79047fac9
Joseph Riascos <josephriascos@gmail.com>
2020-02-11 12:42:28 -0500

    added some javafx code to begin making the default screen, complete with ugly colors to see elements. No Json yet.

--- src/tech/jriascos/application/Window.java
+++ src/tech/jriascos/application/Window.java
@@ -1,78 +1,81 @@
 package tech.jriascos.application;
 
-import javafx.application.*;
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.*;
+import static javafx.geometry.HPos.RIGHT;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.stage.*;
-import javafx.animation.*;
-import javafx.util.*;
- 
+import javafx.stage.Stage;
+
 public class Window extends Application {
-    
+
     @Override
-   public void start(Stage stage) {
-        //Scene and root node setup
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Dictionary Application");
+        primaryStage.setMaximized(true);
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
+        grid.setId("rootGrid");
         grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setGridLinesVisible(true);
 
-        Text sceneTitle = new Text("Welcome");
-        grid.add(sceneTitle, 0, 0, 1, 1);
+        HBox buttonHousing = new HBox();
+        buttonHousing.setId("buttonHousing");
 
-        Label userName = new Label("Username:");
-        grid.add(userName, 0, 1);
+        VBox leftColumn = new VBox();
+        leftColumn.setId("leftColumn");
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        Button addButton = new Button("Add");
+        addButton.setId("addButton");
+        buttonHousing.getChildren().add(addButton);
+        
+        Button removeButton = new Button("Remove");
+        removeButton.setId("removeButton");
+        buttonHousing.getChildren().add(removeButton);
 
-        Label pw = new Label("Password: ");
-        grid.add(pw, 0, 2);
+        TextField searchbar = new TextField();
+        searchbar.setId("searchbar");
+        searchbar.setPromptText("Search");
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        leftColumn.getChildren().add(buttonHousing);
+        leftColumn.getChildren().add(searchbar);
 
-        //grid.setGridLinesVisible(true);
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(15);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(85);
+        grid.getColumnConstraints().addAll(col1,col2);
 
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(100);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
+        addButton.prefWidthProperty().bind(Bindings.divide(buttonHousing.widthProperty(), 2.0));
+        removeButton.prefWidthProperty().bind(Bindings.divide(buttonHousing.widthProperty(), 2.0));
 
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
 
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent e) {
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
-
-        stage.setTitle("JavaFX Tutorial");
+        grid.add(leftColumn, 0, 0);
         Scene scene = new Scene(grid, 300, 275);
-        stage.setScene(scene);
-        String css = this.getClass().getResource("/window.css").toExternalForm();
-        scene.getStylesheets().add(css);
-        stage.show();
-   }
- 
-   public static void main(String[] args) {
-       launch(args);
-   }
+        scene.getStylesheets().add(Window.class.getResource("/window.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    
+
 }
