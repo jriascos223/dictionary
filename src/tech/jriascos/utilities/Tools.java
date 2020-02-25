69b25ed62d6c8f956910c922eae8e8fa8cf74d54
Joseph Riascos <josephriascos@gmail.com>
2020-02-25 16:33:07 -0500

    made correction to getting classpath and words.json file, should work on windows machines now

--- src/tech/jriascos/utilities/Tools.java
+++ src/tech/jriascos/utilities/Tools.java
@@ -7,13 +7,29 @@ public class Tools {
      */
     public static String getClasspathDir() {
         String classpath = System.getProperty("java.class.path", ".");
-        String[] splitClasspathDir = classpath.split(":");
-        String classpathDirectory = "";
-        for (String s : splitClasspathDir) {
-            if (s.matches(".*lib/.*")) {
-                classpathDirectory = s;
-            }
+        boolean windows = false;
+        if (classpath.matches(".*\\\\.*")) {
+            windows = true;
         }
-        return classpathDirectory;
+        if (windows) {
+            String[] splitClasspathDir = classpath.split(";");
+            String classpathDirectory = "";
+            for (String s : splitClasspathDir) {
+                if (s.matches(".*lib\\\\.*")) {
+                    classpathDirectory = s;
+                }
+            }
+            return classpathDirectory;
+        }else {
+            String[] splitClasspathDir = classpath.split(":");
+            String classpathDirectory = "";
+            for (String s : splitClasspathDir) {
+                if (s.matches(".*lib/.*")) {
+                    classpathDirectory = s;
+                }
+            }
+            return classpathDirectory;
+        }
+        
     }    
 }
